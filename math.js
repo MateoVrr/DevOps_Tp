2ce72c50bd4135c616abb1338f244c6c75be1680
Mateo Vrr <mateo.torres@itecriocuarto.org.ar>
2025-08-28 10:29:56 -0300

    agrego ci y test

---
+++ math.js
@@ -0,0 +1,18 @@
+function suma(a, b) {
+  return a + b;
+}
+
+function resta(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+// Esto es necesario para que las pruebas puedan usar estas funciones
+module.exports = {
+  suma,
+  resta,
+  multiplicar
+};
